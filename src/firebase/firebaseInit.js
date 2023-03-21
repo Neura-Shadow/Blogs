a61a636f5cf1363cc9e3fb16538bd4adfabddb3c
Neura-Shadow <86242536+Neura-Shadow@users.noreply.github.com>
2023-03-21 14:18:47 +0800

    Update firebaseInit.js

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -2,12 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2ZJZZZmOlbnco9kaooztR4DBz2Gm5y5U",
-  authDomain: "vueandfb-1c39f.firebaseapp.com",
-  projectId: "vueandfb-1c39f",
-  storageBucket: "vueandfb-1c39f.appspot.com",
-  messagingSenderId: "276834095115",
-  appId: "1:276834095115:web:d6b536f8ee2b92aeb3370e"
+/* here is private key*/
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
